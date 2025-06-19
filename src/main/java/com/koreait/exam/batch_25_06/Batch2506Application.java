0fdc016693180868729a48ca6ab6e6cad4cd3702
hwshin1 <hwshin5044@gmail.com>
2025-06-19 16:34:25 +0900

    기본 세팅

---
+++ src/main/java/com/koreait/exam/batch_25_06/Batch2506Application.java
@@ -0,0 +1,13 @@
+package com.koreait.exam.batch_25_06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Batch2506Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Batch2506Application.class, args);
+    }
+
+}
